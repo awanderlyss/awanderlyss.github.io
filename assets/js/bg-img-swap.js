9d2542ff0014b092f5a3958f317d04a3bceca61e
awanderlyss <alyssa.felix7@gmail.com>
2017-03-05 21:16:40 -0500

    Remove c logs & add comments

--- assets/js/bg-img-swap.js
+++ assets/js/bg-img-swap.js
@@ -3,14 +3,13 @@ $('header').on('click', function(){
   // get the id of the tab with the class is-active
   // http://stackoverflow.com/questions/37988484/how-to-get-id-from-class
   let currentTab = $('.is-active')[1].id;
-  console.log(currentTab);
   // if tab is not equal to home
   if(currentTab !== 'home'){
-    console.log("currentTab is not home!!");
-    // we need to change the background image to white
+    // change the background image to white
     $('.mdl-layout').css('background', 'white');
   }
   else {
+    // home tab is clicked after init load, img will be set
     $('.mdl-layout').css('background', "url('./assets/images/bg-img2.jpg')");
   }
 
