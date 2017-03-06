f29661ca7355a8fc1bafbe0228ed2bae23be0472
awanderlyss <alyssa.felix7@gmail.com>
2017-03-05 20:04:00 -0500

    Set currentTab = to id of active tab

--- assets/js/bg-img-swap.js
+++ assets/js/bg-img-swap.js
@@ -1,3 +1,9 @@
-// check to see what tab is active
-// if tab is not equal to home
-// we need to change the background image to white
+// check to see what tab is active when the header is clicked
+$('header').on('click', function(){
+  // get the id of the tab with the class is-active
+  let currentTab = $('.is-active')[1].id;
+  console.log(currentTab);
+  // if tab is not equal to home
+
+  // we need to change the background image to white
+});
