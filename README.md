856ebafc1b0804da2cd2ccc85e1d7a6e916c93dd
awanderlyss <alyssa.felix7@gmail.com>
2019-10-13 23:00:50 -0400

    Created computer screen animation for large to medium/small screen

--- README.md
+++ README.md
@@ -84,13 +84,14 @@ Background Photo Credit:
   <li>&#9634; Create and Add WebFont Kit to match new resumé</li>
   <li>&#9634; Optimize SVG with SVG OPTIMISER by Peter Collingride</li>
   <li>&#9634; Remove all unnecessary images from version 1</li>
+  <li>&#9634; Minimize js file by create functions for minimal code</li>
 </ul>
 
 <h3>New Branch: <strong>background</strong></h3>
 <ul>
   <li>&#9635; Set-up new working branch: background</li>
   <li>&#9635; Set-up bg image for large displays and background graident for medium/small screens</li>
-  <li>&#9634; Create background animation for large to medium/small</li>
+  <li>&#9635; Create background animation for large to medium/small</li>
   <li>&#9634; Git merge branches with completed</li>
 </ul>
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,24 @@
-// http://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
-// function that capitalizes the first letter of a string
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-};
-
-// self-invoking func to create toolbox on about tab
-(function createToolbox() {
-  // get toolbox div
-  let toolbox = $("#toolbox");
-
-  // array of icons
-  let icons = ["devicon-css3-plain", "devicon-html5-plain", "devicon-javascript-plain", "devicon-ruby-plain", "devicon-angularjs-plain", "devicon-jquery-plain", "devicon-rails-plain", "devicon-sass-original", "devicon-git-plain", "devicon-github-plain",
-  "devicon-heroku-original"];
-
-  // for each icon in icons
-  for(let icon in icons){
-    // create div to hold icon and title
-    let tool = $('<div class="tool"></div>');
-    // split icon name at "-"
-    let splitIconNameArr = icons[icon].split('-');
-    // set icon name equal to correct string
-    let iconName = splitIconNameArr[1];
-    // append icon & icon name in a tool div
-    tool.append(`
-      <i class="${icons[icon]} colored"></i>
-      <p>${iconName.capitalize()}</p>
-    `);
-    // append tool to toolbox div
-    toolbox.append(tool);
+document.addEventListener("DOMContentLoaded", function(event){
+  var bodySize = document.body.getBoundingClientRect().width;
+  console.log(typeof bodySize);
+  function createWebsiteWindow(){
+    bodySize     = document.body.getBoundingClientRect().width;
+    console.log(bodySize)
+    var display  = document.querySelector('#display').getBoundingClientRect()
+        website  = document.querySelector('#website');
+    if(bodySize > 900){
+      website.style.top = display.top+'px';
+      website.style.left = display.left+'px';
+      website.style.right = display.right+'px';
+      website.style.bottom = display.bottom+'px';
+      website.style.width = display.width+'px';
+      website.style.height = display.height+'px';
+    }else{
+      console.log("It's scaling!");
+    }
   }
-})();
+  if (bodySize > 900) {
+    createWebsiteWindow()
+  }
+  window.addEventListener("resize", createWebsiteWindow)
+});
